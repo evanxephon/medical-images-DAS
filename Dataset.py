28682764e67c4f7c07045ffd110e08ce71bf781d
evanxephon <evanxephon@gmail.com>
2019-02-27 19:28:55 +0800

    add binary classification

--- Dataset.py
+++ Dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 import torch
 import numpy as np
 import torch.utils.data
+from sklearn.utils import shuffle
 
 # inherit from torch.utils.data.Dataset to realize myown dataset class
 class MyDataset(torch.utils.data.Dataset):
@@ -30,16 +31,13 @@ def getDummy(dataset):
 
 def maketraindata(num,traindata):
     # sample to get trainset (may not be a necessity)
-    data0 = pd.read_csv(traindata[0]).sample(num)
-    data1 = pd.read_csv(traindata[1]).sample(num)
-    data2 = pd.read_csv(traindata[2]).sample(num)
-    data3 = pd.read_csv(traindata[3]).sample(num)
-    data4 = pd.read_csv(traindata[4]).sample(num)
-
-    traindata = data0.append([data1, data2, data3, data4])
+    traindata = pd.Dataframe()
+    for data in traindata:
+        traindatax = pd.read_csv(data).sample(num)
+        traindata.append(traindatax) 
     return  traindata
 
-def maketestdata(num,rawdata):
+def maketestdata(num,rawdata,binary=False):
 
     # get the testset ,it'd better be balanced so that we can use accuracy only to evaluate the model
     data0 = rawdata[rawdata['label'] == 0]
@@ -47,30 +45,37 @@ def maketestdata(num,rawdata):
     data2 = rawdata[rawdata['label'] == 2]
     data3 = rawdata[rawdata['label'] == 3]
     data4 = rawdata[rawdata['label'] == 4]
-
-    data0test = data0.iloc[-num:, :]
-    data1test = data1.iloc[-num:, :]
-    data2test = data2.iloc[-num:, :]
-    data3test = data3.iloc[-num:, :]
-    data4test = data4.iloc[-num:, :]
-
-    # upsample the raw data to get balanced test data
-    # testdata0 = data0test.loc[np.random.choice(data0test.index,size=num, replace=True),:]
-    # testdata1 = data1test.loc[np.random.choice(data1test.index,size=num, replace=True),:]
-    # testdata2 = data2test.loc[np.random.choice(data2test.index,size=num, replace=True),:]
-    # testdata3 = data3test.loc[np.random.choice(data3test.index,size=num, replace=True),:]
-    # testdata4 = data4test.loc[np.random.choice(data4test.index,size=num, replace=True),:]
-    testdata = data0test.append([data1test,data2test,data3test,data4test])
+    
+    if binary:
+        data1 = shuffle(data0.append([data1,data2,data3,data4]))
+        data1['label'] = 1
+        data0test = data0.iloc[-num:,:]
+        data1test = data1.iloc[-num:,:]
+        testdata = data0test.append(data1test)
+    else:
+        data0test = data0.iloc[-num:, :]
+        data1test = data1.iloc[-num:, :]
+        data2test = data2.iloc[-num:, :]
+        data3test = data3.iloc[-num:, :]
+        data4test = data4.iloc[-num:, :]
+      
+        # upsample the raw data to get balanced test data
+        # testdata0 = data0test.loc[np.random.choice(data0test.index,size=num, replace=True),:]
+        # testdata1 = data1test.loc[np.random.choice(data1test.index,size=num, replace=True),:]
+        # testdata2 = data2test.loc[np.random.choice(data2test.index,size=num, replace=True),:]
+        # testdata3 = data3test.loc[np.random.choice(data3test.index,size=num, replace=True),:]
+        # testdata4 = data4test.loc[np.random.choice(data4test.index,size=num, replace=True),:]
+        testdata = data0test.append([data1test,data2test,data3test,data4test])
 
     return testdata
 
-def config(traindata,testdata):
+def config(traindata,testdata,onehot=True):
     # set the batchsize and the other things
     batch_size = 128
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    train_dataset = MyDataset(getDummy(traindata))
-    test_dataset = MyDataset(getDummy(testdata))
-
+    if onehot:
+        train_dataset = MyDataset(getDummy(traindata))
+        test_dataset = MyDataset(getDummy(testdata))
     # set the dataloader api
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                batch_size=batch_size,
@@ -82,5 +87,5 @@ def config(traindata,testdata):
 
 def getloader(trainnum=100000,testnum=20,rawdata=pd.read_csv('rawdata0sort.csv'),*traindata):
     traindata = maketraindata(trainnum,traindata)
-    testdata = maketestdata(testnum,rawdata)
-    return config(traindata,testdata)
+    testdata = maketestdata(testnum,rawdata,binary=False)
+    return config(traindata,testdata,onehot=True)
