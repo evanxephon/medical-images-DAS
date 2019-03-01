7c90f18798142affc8ab833925155bc61a6c3e52
evanxephon <evanxephon@gmail.com>
2019-03-01 15:58:43 +0800

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -45,37 +45,8 @@ def maketraindata(num,traindata):
     print(traindataset.describe()) 
     return  traindataset
 
-'''def maketestdata(num,rawdata,binary=False):
-
-    # get the testset ,it'd better be balanced so that we can use accuracy only to evaluate the model
-    data0 = rawdata[rawdata['label'] == 0]
-    data1 = rawdata[rawdata['label'] == 1]
-    data2 = rawdata[rawdata['label'] == 2]
-    data3 = rawdata[rawdata['label'] == 3]
-    data4 = rawdata[rawdata['label'] == 4]
-    
-    if binary:
-        data1 = shuffle(data0.append([data1,data2,data3,data4]))
-        data1['label'] = 1
-        data0test = data0.iloc[-num:,:]
-        data1test = data1.iloc[-num:,:]
-        testdata = data0test.append(data1test)
-    else:
-        data0test = data0.iloc[-num:, :]
-        data1test = data1.iloc[-num:, :]
-        data2test = data2.iloc[-num:, :]
-        data3test = data3.iloc[-num:, :]
-        data4test = data4.iloc[-num:, :]
-      
-        # upsample the raw data to get balanced test data
-        # testdata0 = data0test.loc[np.random.choice(data0test.index,size=num, replace=True),:]
-        # testdata1 = data1test.loc[np.random.choice(data1test.index,size=num, replace=True),:]
-        # testdata2 = data2test.loc[np.random.choice(data2test.index,size=num, replace=True),:]
-        # testdata3 = data3test.loc[np.random.choice(data3test.index,size=num, replace=True),:]
-        # testdata4 = data4test.loc[np.random.choice(data4test.index,size=num, replace=True),:]
-        testdata = data0test.append([data1test,data2test,data3test,data4test])
-
-    return testdata'''
+def upsample(data,num):
+    return data = data.loc[np.random.choice(data.index,size=num, replace=True),:]
 
 def config(traindata,testdata,onehot=True):
     # set the batchsize and the other things
@@ -96,7 +67,7 @@ def config(traindata,testdata,onehot=True):
                                               shuffle=False)
     return train_loader,test_loader
 
-def getloader(trainnum,testdata,*traindata):
+def getloader(trainnum,testdata,traindata):
     traindata = maketraindata(trainnum,traindata)
     testdata = pd.read_csv(testdata)
     return config(traindata,testdata,onehot=False)
