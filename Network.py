a6e6d93771553fb2720aaa40856c091377d9901b
evanxephon <evanxephon@gmail.com>
2019-03-26 13:36:59 +0800

    Update Network.py

--- Network.py
+++ Network.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init
 import numpy as np
-
+import Interpretation
 
 class Net(nn.Module):
     #layers is array that contain 3 element，they are l1，l2，l3's input size，l4's output size is 5 (5 types)
@@ -99,7 +99,9 @@ class Net(nn.Module):
     def relprop(self):
         
         relevance_score_of_each_layer = {}
+        
         relevance_score = self.relevance_score_output_layer
+        
         self.tensor_of_each_layer.reverse()
         
         
@@ -112,32 +114,11 @@ class Net(nn.Module):
         for name, param in self.named_parameters():
             if 'fc' in name and 'weight' in name:
                 parameters.append(param.data.cpu().detach().numpy())
-#         print(parameters)
+                
         parameters.reverse()
-#         print(len(parameters))
-#         print(len(self.tensor_of_each_layer))
+
+        relevance_score_of_each_layer = Interpretation.zplus(self.layers, self.tensor_of_each_layer,current_relevance_score,parameters,relevance_score_of_each_layer)
         
-        # caculate each layer's relevance score , 
-        # assuming the input layer dimension is i and output layer dimension is j
-        for i in range(len(self.layers)):
-            
-            # get positive weight
-            positive_weight = np.abs(parameters[i])
-            
-            # after this, we get a j-dim-column-vector, adding the 1e-9 to keep the precision    i-dim-column-vector  *  j*i-matrix
-            sum_posi_weights = np.dot(positive_weight[i], self.tensor_of_each_layer[i]) + 1e-9
-            
-            # this is a numpy element-wise operation, we'll get a j-dim-column-vector            j-dim-column-vector / j-dim-column-vector
-            s_coeffecient = relevance_score / sum_posi_weights
-            
-            # we'll get a i-dim-column-vector                                                    j-dim-column-vector  *   j*i-matrix
-            c_coeffecient = np.dot(positive_weight.T, s_coeffecient)
-            
-            # we get the previous layer's relevance score by using a numpy element-wise operation again   i-dim-v  *   i-dim-v 
-            relevance_score = self.tensor_of_each_layer[i] * c_coeffecient
-            
-            relevance_score_of_each_layer[f'l{len(self.layers) - i}-layer-relevance-score'] = relevance_score
-            
         return relevance_score_of_each_layer
         
     # weight initialization
